1673f76519e5cace56e5e9477974da853de7bd94
paulkav1@gmail.com <paulkav1@gmail.com>
2013-10-05 21:01:41 -0400

    Fully functional

--- cart.js
+++ cart.js
@@ -4,37 +4,27 @@ function formCheck(){
 	if (item == null || item == ""){
 		$('#item1').css("background-color", "yellow");
 		$('#add').css("color", "grey");
-		$('#items').html('<p>Fix the item!</p>');		
-		edit_fail = true;	
+		$('#items').html('<p>Fix the item!</p>');				
 	}else{
+		item = $('#item1').val();
+		items++;
+		item_list[items] = item
+		$('#item1').val('');
+		$('#ct').append('<div><input class="item" type="text" value="' + item + '" name="f1"><input class="chk" type="checkbox"></div>');
 		$('#item1').css("background-color", "white");
 		$('#add').css("color", "black");
-		$('#items').html('<p>' + items + ' items</p>');		
-		edit_fail = false;				
+		$('#items').html('<p>' + items + ' items</p>');							
 	}
 	return false;
 };
 
 $(document).ready(function(){
-	edit_fail = false;
 	items = 0;
 	item_list = [];
 
 	$('#add').click(function(){
-		if (!edit_fail){
-			if (items != 0){
-				item = $('#item1').val();
-				$('#item1').val('');
-			}else{
-				item = '';
-			}			
-			$('#ct').append('<div><input class="item" type="text" value="' + item + '" name="f1" placeholder="enter value"><input class="chk" type="checkbox"></div>');
-			items++;
-			item_list[items] = item
-		}
 	});
 
 	$('.item').change(function(){
-		item = $('#item1').val();
 	});
 })
