ccf743e154c82d78e494953ea118b981dac1e3d6
paulkav1@gmail.com <paulkav1@gmail.com>
2013-10-07 09:01:01 -0400

    itemx

--- cart.js
+++ cart.js
@@ -1,17 +1,17 @@
 function formCheck(){
-	item = $('#item1').val();
+	item = $('#itemx').val();
 
 	if (item == null || item == ""){
-		$('#item1').css("background-color", "yellow");
+		$('#itemx').css("background-color", "yellow");
 		$('#add').css("color", "grey");
 		$('#items').html('<p>Fix the item!</p>');				
 	}else{
-		item = $('#item1').val();
+		item = $('#itemx').val();
 		items++;
 		item_list[items] = item
-		$('#item1').val('');
+		$('#itemx').val('');
 		$('#ct').append('<div><input class="item" type="text" value="' + item + '" name="f1"><input class="chk" type="checkbox"></div>');
-		$('#item1').css("background-color", "white");
+		$('#itemx').css("background-color", "white");
 		$('#add').css("color", "black");
 		$('#items').html('<p>' + items + ' items</p>');							
 	}
@@ -25,6 +25,6 @@ $(document).ready(function(){
 	$('#add').click(function(){
 	});
 
-	$('.item').change(function(){
+	$('.itemx').change(function(){
 	});
 })
